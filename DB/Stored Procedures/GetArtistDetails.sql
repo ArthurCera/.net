f010ea840d67b1685a12b1eaebeea753122e2f99
ArthurCera <arthur.cera@gmail.com>
2023-10-25 18:50:56 -0300

    ADding GetArtistDetails.sql procedure

---
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROC GetArtistDetails
+	@artistID int
+AS
+BEGIN
+	select * from Artist where @artistID = artistID;
+	select * from Album where @artistID = artistID;
+	select * from Song where @artistID = artistID;
+END
